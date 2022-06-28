42a68aae27be558b41072880b9e83643122ed5c0
Vinamra Dash <dashvinamra@gmail.com>
2022-06-28 21:59:56 +0530

    add login page

--- codejudge/home/views.py
+++ codejudge/home/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
+    return render(request,'index.html')
+
+
+def loginUser(request):
+    if(request.method=="POST"):
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        
+    return render(request,'login.html')
+
+
+def logoutUser(request):
+    if not request.user.is_anonymous:
+        logout(request)
+    return redirect("/login")
+ 
