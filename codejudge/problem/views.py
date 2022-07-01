53466140832395dde0edf011f1a59f248055f56a
Vinamra Dash <dashvinamra@gmail.com>
2022-07-02 02:41:27 +0530

    linked submissions model to leaderboad

--- codejudge/problem/views.py
+++ codejudge/problem/views.py
@@ -1,12 +1,23 @@
+from tkinter import S
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from .models import Problem
 
 from .models import Problem
+from leaderboard.models import Submission
 
 # Create your views here.
 @login_required(login_url='loginUser')
 
 def problem(request,prob_id):
+    problem = Problem.objects.get(id=prob_id)
+    if request.method == 'POST':
+        submittedCodeText = request.POST['submittedCodeText']
+        submission = Submission(user=request.user,problem=problem,submitted_code = submittedCodeText)
+        submission.save()
+        print(submission)
+        return redirect('leaderboard')
+
     problem = Problem.objects.get(id=prob_id)
     context = {
         'problem':problem
